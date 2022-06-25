4d27f0739a6db7e2fea28bffa5abafe4178053df
pearsettings44 <jose.a.pereira03@gmail.com>
2022-06-25 22:37:40 +0100

    update webdev project

--- webdev/José Pereira/src/functions/mailingList.js
+++ webdev/José Pereira/src/functions/mailingList.js
@@ -2,9 +2,27 @@ import { createHTMLelement } from "./createElements";
 import { maindiv } from "../index";
 /* Creates a mailing list section */
 function renderMailingList() {
-  const test = createHTMLelement("div", "", "", "")
+  const formDiv = createHTMLelement("div", "formDiv", "", "")
+  /* Mailing list form */
+  const form = createHTMLelement("form", "mail", "", "");
+  const label = createHTMLelement("label", "", "", "join our mailing list");
+  /* input (default type = text) box */
+  const inputBox = createHTMLelement("div", "", "", "");
+  const input = createHTMLelement("input", "", "", "");
+  input.placeholder = "Enter your email address";
+  /* placeholder */
+  const buttonBox = createHTMLelement("div", "", "", "");
+  const button = createHTMLelement("button", "mailBtn", "", "subscribe");
+
+  /* Add everything */
+  inputBox.appendChild(input);
+  buttonBox.appendChild(button);
+  form.appendChild(label);
+  form.appendChild(inputBox);
+  form.appendChild(buttonBox);
+  formDiv.appendChild(form)
   /* Append to the DOM */
-  maindiv.appendChild(test);
+  maindiv.appendChild(formDiv);
 }
 
 export { renderMailingList };
